4f6d6b68666a4e04722db08a74b6374bab0a12f5
milleniumbug <milleniumbug@protonmail.com>
2018-05-31 21:22:47 +0200

    Update Ubuntu

--- i3/.i3/rofi.sh
+++ i3/.i3/rofi.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-rofi -color-enabled true -color-window '#273238, #273238, #1e2529' -color-normal '#273238, #c1c1c1, #273238, #394249, #ffffff' -color-active '#273238, #80cbc4, #273238, #394249, #80cbc4' -color-urgent '#273238, #ff1844, #273238, #394249, #ff1844' -combi-modi run,window -show combi
+rofi -color-enabled true -color-window '#273238, #273238, #1e2529' -color-normal '#273238, #c1c1c1, #273238, #394249, #ffffff' -color-active '#273238, #80cbc4, #273238, #394249, #80cbc4' -color-urgent '#273238, #ff1844, #273238, #394249, #ff1844' -combi-modi run,window -show run
